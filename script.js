eeb91da7a702efe53d0a7ba993ac8b56542884c9
Gotom Golpwana <nja71764@gmail.com>
2025-04-15 10:17:31 +0000

    initialized commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// 1. Given two user profile objects (e.g., personalInfo and contactInfo), merge them into a single userProfile object.
+// 2. Given a product object, log all its keys and values. Bonus: Format them as "key: value" pairs in an array or print them line by line.
+// 3. Create a settings object (e.g., theme: "dark", notifications: true). Freeze it and try changing a value. Use Object.isFrozen to confirm if it's immutable.
+
+// number1
+let user1 ={
+    name:"nenfort",
+    age:21,
+    contact:"070457868"
+}
+let user2 = {
+    addres:"jos",
+    bg:'O+',
+    height:"5.6ft"
+}
+Object.assign(user1,user2)
+console.log(Object.assign(user1,user2));
